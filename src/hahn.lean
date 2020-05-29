a259e601ea477c3092885c3c9146eee434f343e8
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2020-05-30 01:28:25 +0200

    Cleanup hahn.

--- src/hahn.lean
+++ src/hahn.lean
@@ -4,6 +4,7 @@ import analysis.normed_space.hahn_banach
 import .extend
 open complex
 
+
 -- Stelling 2.5.7 (Hahn-Banach, complexe versie).
 --
 -- Zij X (F) een complexe genormeerde ruimte,
@@ -23,25 +24,19 @@ end
 
 lemma b: - I * I = (1: ℂ) := by norm_num
 
-noncomputable def re_of {F' : Type*} [normed_group F'] [normed_space ℂ F'] (f : F' →L[ℂ ] ℂ)
-    : F' →L[ℝ] ℝ
-    := continuous_linear_map.re.comp $ f.restrict_scalars ℝ
-
-lemma re_of_apply {F' : Type*} [normed_group F'] [normed_space ℂ F'] (f : F' →L[ℂ ] ℂ) (x: F')
-    : (re_of f) x = (f x).re
-    := begin
-    dsimp [re_of],
-    refl,
-end
-
 variables  {F : Type*} [normed_group F] [normed_space ℂ F]
+
+noncomputable def re_of (f : F →L[ℂ ] ℂ) : F →L[ℝ] ℝ :=
+continuous_linear_map.re.comp $ f.restrict_scalars ℝ
+
+lemma re_of_apply (f : F →L[ℂ] ℂ) (x: F) : re_of f x = (f x).re := rfl
 
 noncomputable def c (f : F →L[ℂ] ℂ) := λ z: F, ((f z).re : ℂ) - (f (I • z)).re * I
 
 -- (1) We merken op dat φ 0 ∈ L(Y, C) volledig bepaald is door Re φ 0 : omdat
 --     φ 0 (iy) = iφ 0 (y), is Im φ 0 (y) = − Re φ 0 (iy) voor elke y ∈ Y .
-lemma xx (f : F →L[ℂ] ℂ) :
-  ∀ z, f z = c f z := begin
+lemma xx (f : F →L[ℂ] ℂ) : ∀ z, f z = c f z :=
+begin
     intro,
     rw c,
     ext,
@@ -58,23 +53,9 @@ lemma xx (f : F →L[ℂ] ℂ) :
     ... = - (f (I • z)).re : by rw [←neg_mul_eq_neg_mul, neg_im, a'],
 end
 
--- (2) Bovendien is Re φ 0 ∈ L(Y, R): Y is een C-vectorruimte, en dus ook een
---     R-vectorruimte.
---     De R-lineariteit van Re φ 0 volgt eenvoudig uit de C-lineariteit van φ 0 .
---          reestrict_scalars
---     Omdat |Re z| ≤ |z|, voor elke z ∈ C, is ∥Re φ 0∥L(Y,R) ≤ ∥φ 0∥L(Y,C) .
-lemma y (f : F →L[ℂ] ℂ) : ∥re_of f∥ ≤ ∥f∥ := begin
-    have := continuous_linear_map.op_norm_comp_le
-        continuous_linear_map.re
-        (f.restrict_scalars ℝ),
-    rw [complex.continuous_linear_map.re_norm, one_mul] at this,
-    exact this,
-end
-
 noncomputable def restrict_scalars
-    {G : Type*} [normed_group G] [normed_space ℂ G] (X: subspace ℂ G)
-    : subspace ℝ G
-    :=
+    {G : Type*} [normed_group G] [normed_space ℂ G] (X: subspace ℂ G) :
+    subspace ℝ G :=
 {
     carrier := X.carrier,
     zero := X.zero,
@@ -97,50 +78,43 @@ noncomputable def restrict_scalars
 --          f = Re φ 0 = re_of φ 0 : p →L[ℝ] ℝ
 --          p = Y ≤_ℂ X = F
 --          → φ r = g: F →L[ℝ] ℝ
-lemma z (p : subspace ℂ F) (f : p →L[ℂ] ℂ) : 
-  ∃ g : F →L[ℝ] ℝ, (∀ x : (restrict_scalars p), g x = (re_of f) x) ∧ ∥g∥ = ∥re_of f∥ := begin
-    exact exists_extension_norm_eq (p.restrict_scalars ℝ) (re_of f),
+lemma z (p : subspace ℂ F) (f' : p →L[ℝ] ℝ) : 
+  ∃ g : F →L[ℝ] ℝ, (∀ x : (restrict_scalars p), g x = f' x) ∧ ∥g∥ = ∥f'∥ := begin
+    exact exists_extension_norm_eq (p.restrict_scalars ℝ) f',
 end
 
-lemma d (p : subspace ℂ   F) (f : p →L[ℂ] ℂ) :
+lemma d (p : subspace ℂ F) (f : p →L[ℂ] ℂ) :
     ∃ g : F →L[ℂ] ℂ, (∀ x : p, g x = f x) ∧ ∥g∥ = ∥f∥
     := begin
-    have := z p f,
-    cases this with g hg,
-    use continuous_linear_map.extend_to_C g,
+    have := z p (re_of f),
+    rcases this with ⟨g, ⟨hextends, hnormeq⟩⟩,
+    use g.extend_to_C,
     have : ∀ (x : p), (g.extend_to_C) x = f x,
     {
         intros,
-        have := hg.1 x,
-        have ttt := xx f x,
-        rw ttt,
-        dsimp [c],
-        rw ←re_of_apply,
-        rw ←this,
         let ix : ↥p := I • x,
-        rw ← (show ix = I • x, by refl),
-        rw ←re_of_apply,
-        rw ←(hg.1 ix),
-        rw mul_comm,
-        refl,
+        calc g.extend_to_C x
+            = (g x) - I * (g ix) : rfl
+        ... = (g x) - (g ix) * I : by rw mul_comm
+        ... = (g x) - ((re_of f) (I • x)) * I : by rw hextends ix
+        ... = (g x) - ((f (I • x)).re) * I : by rw re_of_apply
+        ... = ((re_of f) x) - ((f (I • x)).re) * I : by rw hextends x
+        ... = ((f x).re) - ((f (I • x)).re) * I : by rw re_of_apply
+        ... = c f x : by refl
+        ... = f x :  (xx f x).symm,
     },
 
     split,
     assumption,
 
     refine le_antisymm _ _,
-    have := continuous_linear_map.op_norm_le_bound
-        (@continuous_linear_map.extend_to_C _ _ _ (g))
-        (continuous_linear_map.op_norm_nonneg (g))
-        (norm_bound _),
-    calc  ∥g.extend_to_C∥ ≤ ∥g∥ : this
-    ... = ∥re_of f∥ : hg.2
-    ... ≤ ∥f∥ : y _,
+    {
+        calc ∥g.extend_to_C∥
+            ≤ ∥g∥ : g.extend_to_C.op_norm_le_bound g.op_norm_nonneg (norm_bound _)
+        ... = ∥re_of f∥ : hnormeq
+        ... ≤ ∥continuous_linear_map.re∥ * ∥f∥ : continuous_linear_map.op_norm_comp_le _ _
+        ... = ∥f∥ : by rw [complex.continuous_linear_map.re_norm, one_mul],
+    },
 
-    refine continuous_linear_map.op_norm_le_bound _ _ _,
-    exact continuous_linear_map.op_norm_nonneg _,
-
-    intros,
-    rw ←this,
-    exact  continuous_linear_map.le_op_norm _ _,
+    exact f.op_norm_le_bound g.extend_to_C.op_norm_nonneg (λ x, this x ▸ g.extend_to_C.le_op_norm x),
 end
